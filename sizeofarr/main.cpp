4fb3e1909f333d7ebc16a985e75201ed0b463bc1
Bob Jiang <dashaomai@gmail.com>
2016-05-10 10:49:18 +0800

    添加了 sizeof 的测试示例

---
+++ sizeofarr/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+  int arr[10];
+
+  cout << "sizeof int[10] = " << sizeof(arr) << endl;
+  cout << "sizeof int = " << sizeof(int) << endl;
+  return 0;
+}
